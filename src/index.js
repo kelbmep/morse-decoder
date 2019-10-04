fab1622eda5d78e9659023e9d6e73a55e8e5e76a
Pavel Grudinskiy <mr.pv234@mail.ru>
2019-10-04 20:32:57 +0300

    completed task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = '';
+    let result = '';
+    for (let index = 0; index < expr.length / 10; index++) {
+        let element = expr.substring(index * 10 + 0, index * 10 + 10);
+        arr = '';
+        for (let index1 = 0; index1 < 5; index1++) {
+            if(element[2 * index1] == '0')
+                continue;
+            else if (element[2 * index1] == '1')
+            {
+                if(element[2 * index1 + 1] == '0')
+                    arr += '.';
+                else 
+                    arr += '-';
+            }
+            else if(element[2 * index1] == '*')
+            {
+                arr = ' ';
+                index1 = 5;
+            }
+        }
+    if(arr == ' ')
+        result += arr;
+    else
+        result += MORSE_TABLE[arr];
+    }
+    return result;
 }
 
 module.exports = {
